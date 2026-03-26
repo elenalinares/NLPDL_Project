3242aa8ead7694cec65d267aa6325f6037dc06e2
elenalinares <elenalinares06@gmail.com>
2026-03-26 12:57:53 +0100

    just the basic code for the data loader and train model

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,34 @@
+def read_iob2(file_path):
+    sentences = []
+    labels = []
 
+    current_sentence = []
+    current_labels = []
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+
+            if line == "":
+                if current_sentence:
+                    sentences.append(current_sentence)
+                    labels.append(current_labels)
+                    current_sentence = []
+                    current_labels = []
+
+            elif line.startswith("#"):
+                continue
+
+            else:
+                parts = line.split("\t")
+
+                if len(parts) < 3:
+                    continue
+
+                word = parts[0]
+                tag = parts[2]
+
+                current_sentence.append(word)
+                current_labels.append(tag)
+
+    return sentences, labels
--- src/train.py
+++ src/train.py
@@ -1,1 +1,30 @@
+from data_loader import read_iob2
 
+train_path = "data/en_ewt-ud-train.iob2"
+
+sentences, labels = read_iob2(train_path)
+
+print("Number of sentences:", len(sentences))
+print("First sentence:", sentences[0])
+print("First labels:", labels[0])
+
+
+# to flatten all labels into one list
+all_labels = [label for sentence in labels for label in sentence]
+
+# we get unique labels
+unique_labels = sorted(list(set(all_labels)))
+
+print("Unique labels:", unique_labels)
+
+label2id = {label: i for i, label in enumerate(unique_labels)}
+id2label = {i: label for label, i in label2id.items()}
+
+print(label2id)
+
+encoded_labels = [
+    [label2id[label] for label in sentence]
+    for sentence in labels
+]
+
+print("Example encoded labels:", encoded_labels[0])
