e6bf4eb7029a67c5833707e14d1590c4a91dc3c2
elenalinares <elenalinares06@gmail.com>
2026-03-27 15:18:58 +0100

    tokenizer added in the train file

--- src/train.py
+++ src/train.py
@@ -1,4 +1,7 @@
 from transformers import BertTokenizerFast
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
+print(tokenizer.tokenize("playing football"))
+
 
 from data_loader import read_iob2
 
@@ -30,3 +33,40 @@ encoded_labels = [
 ]
 
 print("Example encoded labels:", encoded_labels[0])
+
+# TOKENIZE YOUR SENTENCES
+tokenized_inputs = tokenizer(
+    sentences,
+    is_split_into_words=True,
+    padding=True,
+    truncation=True
+)
+
+print("Keys:", list(tokenized_inputs.keys())[:10])
+print("First input_ids:", tokenized_inputs["input_ids"][0])
+
+aligned_labels = []
+
+for i, sentence_labels in enumerate(encoded_labels):
+    word_ids = tokenized_inputs.word_ids(batch_index=i)
+
+    previous_word_idx = None
+    label_ids = []
+
+    for word_idx in word_ids:
+        if word_idx is None:
+            label_ids.append(-100)  # ignore padding & special tokens
+
+        elif word_idx != previous_word_idx:
+            label_ids.append(sentence_labels[word_idx])
+
+        else:
+            # same word split into subwords
+            label_ids.append(sentence_labels[word_idx])
+
+        previous_word_idx = word_idx
+
+    aligned_labels.append(label_ids)
+
+print("Tokenized input_ids:", tokenized_inputs["input_ids"][0])
+print("Aligned labels:", aligned_labels[0])
